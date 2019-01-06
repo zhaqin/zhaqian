f231d3cac8e96940e8a65a03fc13943ef3e516d2
myiszhang <990649132@qq.com>
2019-01-06 20:09:05 +0800

    博客

---
+++ static/js/delBaidu_i.js
@@ -0,0 +1,9 @@
+// var DivId = document.getElementById('page');
+// var Class_i = document.getElementsByClassName('c-icon c-icon-bear-p');
+// DivId.removeClass_i(Class_i);
+
+var parent=document.getElementById("page");
+var child=document.getElementsByClassName("c-icon c-icon-bear-p");
+child.removeAttribute("class");
+
+document.getElementsByClassName('c-icon c-icon-bear-p').className = 'ljbaidu';
